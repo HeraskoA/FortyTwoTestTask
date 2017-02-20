9e2c2121c0c8ee63d13fed37457e748f4056cc5b
Herich <andrey.herasko@gmail.com>
2017-02-20 23:40:33 +0200

    tichet:1 check barista migrations

---
+++ apps/hello/migrations/0002_data.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        from django.core.management import call_command
+        call_command("loaddata", "data.json")
+
+        # Note: Don't use "from appname.models import ModelName". 
+        # Use orm.ModelName to refer to models in this application,
+        # and orm['appname.ModelName'] for models in other applications.
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        u'hello.userdata': {
+            'Meta': {'object_name': 'MyData'},
+            'bio': ('django.db.models.fields.TextField', [], {'max_length': '256'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '40'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {'max_length': '256'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '40'})
+        }
+    }
+
+    complete_apps = ['hello']
+    symmetrical = True
