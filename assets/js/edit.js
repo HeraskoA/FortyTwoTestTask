94a290c3b43ea05172f9a104978d4eb872a8a313
Herich <andrey.herasko@gmail.com>
2017-03-03 17:57:31 +0200

    ticket:13 update edit.js(for 5 ticket)

--- assets/js/edit.js
+++ assets/js/edit.js
@@ -1,45 +1,5 @@
-function scroll() {
-    $('html, body').animate({scrollTop: 0},500);
-    return false;
-}
-
-function display_errors(errors){
-	$('.error').remove();
-	for (var k in errors) {
-		$('form').find('input[name=' + k + ']').attr("class", "form-control has-error");
-		$('form').find('input[name=' + k + ']').after('<div color="red" style="float: right;" class="error" id=' + k + '><font color="red">' + errors[k] + '</font></div>');
-	}
-}
-
-function form(){
-	$('form').ajaxForm({
-		'beforeSend': function(xhr, settings){
-			$('#indicator').show();
-			$('.btn-primary').addClass('hidden');
-                	$('.btn-default').addClass('hidden');
-                	$("input").attr("disabled", "disabled");
-                	$("textarea").attr("disabled", "disabled");
-			},
-		'error': function() {alert('error');},
-		'success': function(data, status, xhr) {
-				$('#indicator').hide();
-				$("input").removeAttr("disabled");
-            			$("textarea").removeAttr("disabled");
-				$('.btn-primary').removeClass('hidden');
-                		$('.btn-default').removeClass('hidden');
-				if (data != 1) {
-					$('#saved').hide();
-					display_errors(data);
-					scroll();
-				}else{
-				$('.error').remove();
-				$('#saved').show();
-				}
-		}
-	});
-}
-
-function ImagePreview(input) {
+$(document).ready(function(){
+    function ImagePreview(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
@@ -49,11 +9,49 @@ function ImagePreview(input) {
         };
         reader.readAsDataURL(input.files[0]);
     }
-}
+    }
 
-$(document).ready(function(){
-form();
- $("#id_photo").change(function(){
+    function form(){
+    $('form').ajaxForm({
+        'beforeSend': function(xhr, settings){
+            $('#indicator').show();
+            $('.btn-primary').addClass('hidden');
+                    $('.btn-default').addClass('hidden');
+                    $("input").attr("disabled", "disabled");
+                    $("textarea").attr("disabled", "disabled");
+            },
+        'error': function() {alert('error');},
+        'success': function(data, status, xhr) {
+                $('#indicator').hide();
+                $("input").removeAttr("disabled");
+                        $("textarea").removeAttr("disabled");
+                $('.btn-primary').removeClass('hidden');
+                        $('.btn-default').removeClass('hidden');
+                if (data != 1) {
+                    $('#saved').hide();
+                    display_errors(data);
+                    scroll();
+                }else{
+                $('.error').remove();
+                $('#saved').show();
+                }
+        }
+    });
+    }
+
+    function display_errors(errors){
+        $('.error').remove();
+        for (var k in errors) {
+            $('form').find('input[name=' + k + ']').attr("class", "form-control has-error");
+            $('form').find('input[name=' + k + ']').after('<div color="red" style="float: right;" class="error" id=' + k + '><font color="red">' + errors[k] + '</font></div>');
+        }
+    }
+    function scroll() {
+        $('html, body').animate({scrollTop: 0},500);
+        return false;
+    }
+    form();
+    $("#id_photo").change(function(){
         ImagePreview(this);
-  });
+    });
 })
