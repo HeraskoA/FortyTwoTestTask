c91e4a248e42cde04f90dbf03aea2f873b23b77b
Herich <andrey.herasko@gmail.com>
2017-03-06 15:10:46 +0200

    ticket:8 fix migrations

--- apps/hello/migrations/0003_superuser.py
+++ apps/hello/migrations/0003_superuser.py
@@ -8,8 +8,6 @@ class Migration(DataMigration):
 
     def forwards(self, orm):
         "Write your forwards methods here."
-        from django.core.management import call_command
-        call_command("loaddata", "superuser.json")
         # Note: Don't use "from appname.models import ModelName". 
         # Use orm.ModelName to refer to models in this application,
         # and orm['appname.ModelName'] for models in other applications.
